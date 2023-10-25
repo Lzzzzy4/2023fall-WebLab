bcedc504cae0bb8d67594b37066b06d7d7d1c8da
Dashes <fangchizheng@mail.ustc.edu.cn>
2023-10-25 10:30:25 +0800

    123

--- src/skip_list.py
+++ src/skip_list.py
@@ -1,26 +1,30 @@
 import random
 
 class ListNode:
-    def __init__(self, val):
+    def __init__(self, val, next = None, down = None):
         self.val = val
-        self.next = None
-    
+        self.next = next
+        self.down = down 
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert(self, val):
+    def insert(self, val, cur = None):
         if self.head is None:
-            self.head = ListNode(val)
-            return
-        node = ListNode(val)
-        cur = self.head
+            node = ListNode(val)
+            self.head = node
+            return (None, node)
+
+        if cur != None:
+            cur = self.head
+
         while cur.next is not None and cur.next.val < val:
             cur = cur.next
 
-        node.next = cur.next
+        node = ListNode(val, cur.next)
         cur.next = node
-    
+        return (cur, node)
+
     def merge_and(self, lst):
         i = self.head
         j = lst.head
@@ -73,14 +77,25 @@ class SkipList:
         self.lists = [LinkedList() for i in range(level)]
         self.p = 0.5
 
+    def rand_level(self):
+        level = 1
+        while random.random() < self.p and level < self.level:
+            level += 1
+        return level
+
     def insert(self, val):
-        first = 0
-        for lst in self.lists:
-            if first == 0 or random.random() > self.p:
-                first = 1
-                lst.insert(val)
-            else:
-                break
+        rlevel = self.rand_level()
+        pre = None
+        cur = None
+        for i in range(rlevel, 0, -1):
+            (nxtpre, nxtcur) = self.lists[i].insert(val, pre)
+            if cur != None:
+                cur.down = nxtcur
+
+            if nxtpre != None:
+                nxtpre = nxtpre.down
+            
+            (pre, cur) = (nxtpre, nxtcur)
     
     #merge with and method
     def merge_and(self, lst):
