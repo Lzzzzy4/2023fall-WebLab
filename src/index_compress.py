45df677eec3ee400b7f2e230754f3b1146d6d207
Lzzzzy4 <lzy1522659928@mail.ustc.edu.cn>
2023-10-27 12:02:32 +0800

    block compress

--- src/index_compress.py
+++ src/index_compress.py
@@ -14,7 +14,54 @@ class map_frefix_compress:
                 cnt += 1
             self.trie[id]['tags'] = words_trie
     
-    def getwords(self) -> list:
-        return self.trie.keys()
-        
-        
+    def get_tags(self, id:int) -> list:
+        return self.trie[id]['tags'].keys()
+    
+    def get_content(self, id:int) -> dict:
+        return self.trie[id]
+
+class map_block_compress:
+    def __init__(self, k:int) -> None:
+        self.map = {}
+        self.k = k
+
+    def compress(self, content) -> None:
+        i = 0
+        block = []
+        firstid = 0
+        for id in content:
+            if i == 0:
+                firstid = id
+            block[i] = content[id]
+            if i == self.k-1:
+                self.map[firstid] = block
+                block = {}
+                i = 0
+
+    def bisearch(self, nums:list, val:int) -> int:
+        # 返回第一个小于等于val的值
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] > val:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return nums[right]
+
+    def get_tags(self, id:int) -> list:
+        block = self.map[self.bisearch(self.map.keys(), id)]
+        for x in block:
+            if x['id'] == id:
+                return x['tags'].split('/')
+        print('id not found')
+        return None
+    
+    def get_content(self, id:int) -> dict:
+        block = self.map[self.bisearch(self.map.keys(), id)]
+        for x in block:
+            if x['id'] == id:
+                return x
+        print('id not found')
+        return None
